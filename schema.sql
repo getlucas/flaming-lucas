ac6e8ac2f1a603802c3a3f2e810bd9af57dd6c1b
Vladimir <volkvid@yandex.ru>
2014-12-30 16:53:43 +0300

    Create schema.sql
    db scheme

---
+++ schema.sql
@@ -0,0 +1,14 @@
+drop table if exists userinfo;
+create table userinfo (
+  user_id integer primary key autoincrement,
+  username text not null,
+  password text not null,
+  status text not null
+);
+drop table if exists tokens;
+create table tokens (
+  user_id integer,
+  token text not null,
+  permission text not null,
+  expiration integer not null
+);
